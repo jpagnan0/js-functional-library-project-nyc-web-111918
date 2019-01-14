a268073943a5f2f40fe647c038d4101fd15c271d
jpagnan0 <a.jpagnano@gmail.com>
2019-01-14 17:58:30 -0500

    up to find

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
-fi = (function() {
-  return {
-    libraryMethod: function() {
-      return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
-    },
+fi = (function () {
+	return {
+		libraryMethod: function () {
+			return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
+		},
 
-    each: function() {
+		each: function (collection, callback) {
+			if (collection instanceof Object && collection instanceof Array) {
+				for (let i = 0; i < collection.length; i++) {
+					callback(collection[i])
+				}
+			} else if (collection instanceof Object && !(collection instanceof Array)) {
+				for (let item in collection) {
+					callback(collection[item])
+				}
+			}
+			return collection
+		},
 
-    },
+		map: function (collection, callback) {
+			const mappedArray = []
+			for (let key in collection) {
+				mappedArray.push(callback(collection[key]))
+			}
+			return mappedArray
+		},
 
-    map: function() {
+		reduce: function (array, combine, acc = 0) {
+			let current = acc;
+			// console.log(current)
+			for (let element of array) {
+				current = combine(current, element);
+				// console.log(current)
+			}
+			return current;
+		},
+		cmp: (a, b) => {
+			return a - b;
+		},
 
-    },
+		find: function (list, value) {
+			
+		},
+		functions: function () {
 
-    reduce: function() {
-
-    },
-    
-    functions: function() {
-
-    },
+		},
 
 
-  }
+	}
 })()
 
 fi.libraryMethod()
